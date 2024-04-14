31396c5bcce76ba263cb8457cc8c0896874ebd1b
bfd <inyakin97@gmail.com>
2024-04-14 12:12:19 +0300

    finish part 1

---
+++ 1/10_deque.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+messages = deque([1, 2, 3, 4, 5])
+
+def process_message(msg):
+    pass
+
+
+while messages:
+    if messages:
+        message = messages.pop()
+        process_message(message)
---
+++ 1/11_tasks.py
@@ -0,0 +1,11 @@
+
+
+def make_request():
+    print('cpu_bound_setup()')
+    print('io_bound_web_request()')
+    print('cpu_bound_postprocess()')
+
+
+task_one = make_request()
+task_two = make_request()
+task_three = make_request()
